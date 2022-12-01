bbce680c50a8ad62654c333e74a7b233080e3e64
paleumm <zdotqdot@gmail.com>
2022-12-01 23:51:37 +0700

    Update zanim-cli.rs

--- zanim/src/zanim-cli.rs
+++ zanim/src/zanim-cli.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "zanim",
+    about = "This is a tool to wrap all the cli management tools"
+)]
+struct Opt {
+    #[structopt(name = "tools", short)]
+    tool: String,
+    #[structopt(name = "mode", short)]
+    mode: String,
+}
+fn main() {
+    let opt = Opt::from_args();
+    let mode = &opt.mode[..];
+}
